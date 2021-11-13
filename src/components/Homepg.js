ec65cd28693bca10c47c01bf7502cbb79ccd2e38
Modsia16 <84951325+Modsia16@users.noreply.github.com>
2021-11-12 19:00:47 -0800

    first commit

---
+++ src/components/Homepg.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Homepg() {
+
+    //can add styles here
+    // const style = {
+    //     backgroundColor: '#f4f4f4',
+    //     height: '100vh',
+    //     width: '100vw',
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //     justifyContent: 'center',
+    //     fontFamily: 'sans-serif',
+    //     fontSize: '2rem',
+    //     color: '#333',
+    //     textAlign: 'center',
+    // } add style={style} to the return statement div if directly using style
+    return (
+        <div className="container">
+        <h1>Home Page</h1>
+        <p>This is the home page</p>
+        </div>
+    );
+    }
