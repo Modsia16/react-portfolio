eed24a4a81f4c1db5775bde4fc208032d952c3a2
Modsia16 <84951325+Modsia16@users.noreply.github.com>
2021-11-17 21:10:39 -0800

    header working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from 'styled-components';
+import { MdMenu, MdClose } from 'react-icons/md';
 
+//nav bar style and function from, root folder in GlobalStyle.js
 const NavBarStyle = styled.div`
     background: var(--blue);
     z-index: 100;
@@ -10,7 +12,7 @@ const NavBarStyle = styled.div`
     width: 100%;
     padding: 1rem 0;
     position: fixed;
-    h1 {
+     .logo {
         color: var(--white);
         font-size: 2rem;
         font-weight: bold;
@@ -22,7 +24,6 @@ const NavBarStyle = styled.div`
         margin: 0 auto;
         width: 90%;
         text-align: center;
-    }
     li {
         display: inline-block;
         border-right: 8px solid var(--blue);
@@ -31,6 +32,7 @@ const NavBarStyle = styled.div`
         &:hover {
             background: var(--light-blue);
         }
+    }
     a {
         color: var(--yellow);
         text-decoration: none;
@@ -42,28 +44,113 @@ const NavBarStyle = styled.div`
     .active {
         color: var(--white);
     }
+}
+    .mobile-menu-icon {
+        position: absolute;
+        right: 0rem;
+        top: 1rem;
+        width: 3rem;
+        cursor: pointer;
+        display: none;
+        outline: none;
+        * {
+            pointer-events: none;
+        }
+    }
+    .closeNavIcon {
+        display: none;
+    }
+    @media only screen and (max-width: 768px) {
+        padding: 0;
+        .hide-item {
+            transform: translateY(calc(-100% - var(--top)));
+        }
+        .mobile-menu-icon {
+            display: block;
+            color: var(--blue);
+        }
+        .navItems {
+            --top: 1rem;
+            transition: 0.3s transform ease-in-out;
+            background: var(--blue);
+            padding: 2rem;
+            width: 90%;
+            max-width: 300px;
+            border-radius: 12px;
+            position: absolute;
+            right: 1rem;
+            top: var(--top);
+            .closeNavIcon {
+                display: block;
+                width: 3rem;
+                margin: 0 0 0 auto;
+                cursor: pointer;
+                color: var(--white);
+                * {
+                    pointer-events: none;
+                }
+            }
+                li {
+                    display: block;
+                    margin-bottom: 1rem;
+                }
+            }
+        }
 `;
 
 //NavLink from react router dom to create a link that is active when the user is on that page
 export default function NavBar() {
+    //add mobile screen function to the navbar to toggle, tab for key down event and hide items after clicked
+    const[showNav, setShowNav] = React.useState(false);
     return(
         <NavBarStyle>
-            <h1>CarilyWebDev</h1>
-            <ul>
+            <div className="mobile-menu-icon" onClick={() => setShowNav(!showNav)}
+                role="button"
+                onKeyDown={() => setShowNav(!showNav)}
+                tabIndex={0}>
+                <MdMenu />
+            </div>
+            <ul className={!showNav ? "navItems hide-item" : "navItems"}>
+                <div className="closeNavIcon" onClick={() => setShowNav(!showNav)}
+                role="button"
+                onKeyDown={() => setShowNav(!showNav)}
+                tabIndex={0}>
+                    <MdClose />
+                </div>
                 <li>
-                    <NavLink to="/" exact>Home</NavLink>
+                    <NavLink to="/" exact onClick={() => 
+                    setShowNav(!showNav)}
+                    role="button"
+                    onKeyDown={() => setShowNav(!showNav)}
+                    tabIndex={0}>Home</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/about">About Me</NavLink>
+                    <NavLink to="/about" onClick={() => 
+                    setShowNav(!showNav)}
+                    role="button"
+                    onKeyDown={() => setShowNav(!showNav)}
+                    tabIndex={0}>About Me</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/projects">Portfolio</NavLink>
+                    <NavLink to="/projects" onClick={() => 
+                    setShowNav(!showNav)}
+                    role="button"
+                    onKeyDown={() => setShowNav(!showNav)}
+                    tabIndex={0}>Portfolio</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/contact" onClick={() => 
+                    setShowNav(!showNav)}
+                    role="button"
+                    onKeyDown={() => setShowNav(!showNav)}
+                    tabIndex={0}>Contact</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/resume">Resume</NavLink>
+                    <NavLink to="/resume"onClick={() => 
+                    setShowNav(!showNav)}
+                    role="button"
+                    onKeyDown={() => setShowNav(!showNav)}
+                    tabIndex={0} >Resume</NavLink>
                 </li>
             </ul>
         </NavBarStyle>
